65d7d8b80adef6ba21a5f023eb9947c6d8ece734
baldflavor <207648894+baldflavor@users.noreply.github.com>
2025-05-20 15:25:16 -0700

    formatting

--- Praxis.Logging/Constant.cs
+++ Praxis.Logging/Constant.cs
@@ -3,20 +3,19 @@ namespace Praxis.Logging;
 /// <summary>
 /// Class that holds constant values
 /// </summary>
-public static class Constant
-{
-    /// <summary>
-    /// Constant string used for property designation on a logger for caller information
-    /// </summary>
-    public const string CALLER = "Caller";
+public static class Constant {
+	/// <summary>
+	/// Constant string used for property designation on a logger for caller information
+	/// </summary>
+	public const string CALLER = "Caller";
 
-    /// <summary>
-    /// Constant string used for property designation on a logger for extra data properties
-    /// </summary>
-    public const string DATA = "Data";
+	/// <summary>
+	/// Constant string used for property designation on a logger for extra data properties
+	/// </summary>
+	public const string DATA = "Data";
 
-    /// <summary>
-    /// Constant string for the initialization message of a log
-    /// </summary>
-    public const string INITIALIZED = "Log Initialized";
+	/// <summary>
+	/// Constant string for the initialization message of a log
+	/// </summary>
+	public const string INITIALIZED = "Log Initialized";
 }
--- Praxis.Logging/Extension.cs
+++ Praxis.Logging/Extension.cs
@@ -6,20 +6,18 @@ using NLog;
 /// <summary>
 /// Extensions for NLog / Logger operations
 /// </summary>
-public static class Extension
-{
-    /// <summary>
-    /// Logs a message indicating initialization. This is most useful as the first entry added when the application domain is starting
-    /// </summary>
-    /// <param name="logger">Logger to use for writing out initialization message</param>
-    /// <param name="initInfo">Information about the domain where the application started</param>
-    /// <param name="callerMemberName">Captures the name of the method where this method was called. Filled in by the caller defaultly using compiler services</param>
-    /// <exception cref="Exception">Thrown if the logging operation could not be completed</exception>
-    public static void Initialized(this Logger logger, InitializeInfo initInfo, [CallerMemberName] string callerMemberName = "")
-    {
-        logger
-            .WithProperty(nameof(InitializeInfo), initInfo)
-            .WithProperty(Constant.CALLER, callerMemberName)
-            .Info(Constant.INITIALIZED);
-    }
+public static class Extension {
+	/// <summary>
+	/// Logs a message indicating initialization. This is most useful as the first entry added when the application domain is starting
+	/// </summary>
+	/// <param name="logger">Logger to use for writing out initialization message</param>
+	/// <param name="initInfo">Information about the domain where the application started</param>
+	/// <param name="callerMemberName">Captures the name of the method where this method was called. Filled in by the caller defaultly using compiler services</param>
+	/// <exception cref="Exception">Thrown if the logging operation could not be completed</exception>
+	public static void Initialized(this Logger logger, InitializeInfo initInfo, [CallerMemberName] string callerMemberName = "") {
+		logger
+				.WithProperty(nameof(InitializeInfo), initInfo)
+				.WithProperty(Constant.CALLER, callerMemberName)
+				.Info(Constant.INITIALIZED);
+	}
 }
