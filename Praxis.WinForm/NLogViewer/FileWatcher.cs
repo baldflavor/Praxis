6307b5be8e169ee51dc01c692bcca47c434a36ae
baldflavor <207648894+baldflavor@users.noreply.github.com>
2025-07-25 15:24:15 -0700

    FileWatcher added to have recursive directory / file search

--- Praxis.WinForm/NLogViewer/FileWatcher.cs
+++ Praxis.WinForm/NLogViewer/FileWatcher.cs
@@ -36,19 +36,19 @@ internal class FileWatcher : PeriodicTimerContainer {
 
 
 	/// <summary>
-	/// Creates an instance of the <see cref="FileWatcher"/> class
+	/// Creates an instance of the <see cref="FileWatcher"/> class.
 	/// </summary>
-	/// <param name="directory">The directory to watch</param>
-	/// <param name="tickFrequency">How often to process file changes</param>
-	/// <param name="startupDelay">Delay in initial startup to process changes / existing files</param>
-	/// <param name="pollingLoopOnException">Delegate for errors during the polling loop</param>
-	/// <param name="processLine">Used to signal that there is a line of text to be processed</param>
-	/// <param name="startingRead">Delegate called before reading lines from files</param>
-	/// <param name="finishedRead">Delegate called after reading lines from files has finished</param>
+	/// <param name="directory">The directory to watch. Subdirectories underneath this directory are also watched.</param>
+	/// <param name="tickFrequency">How often to process file changes.</param>
+	/// <param name="startupDelay">Delay in initial startup to process changes / existing files.</param>
+	/// <param name="pollingLoopOnException">Delegate for errors during the polling loop.</param>
+	/// <param name="processLine">Used to signal that there is a line of text to be processed.</param>
+	/// <param name="startingRead">Delegate called before reading lines from files.</param>
+	/// <param name="finishedRead">Delegate called after reading lines from files has finished.</param>
 	public FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan startupDelay, Action<Exception> pollingLoopOnException, Action<string> processLine, Action startingRead, Action finishedRead) : base(tickFrequency, startupDelay, pollingLoopOnException) {
-		_fileTargets = [.. Directory.GetFiles(directory).Select(f => new FileTarget(f))];
+		_fileTargets = [.. Directory.GetFiles(directory, "*", new EnumerationOptions { RecurseSubdirectories = true }).Select(f => new FileTarget(f))];
 
-		_fsw = new FileSystemWatcher(directory);
+		_fsw = new FileSystemWatcher(directory) { IncludeSubdirectories = true };
 		_fsw.Created += (s, e) => {
 			lock (_fileTargets)
 				_fileTargets.Add(new FileTarget(e.FullPath));
@@ -71,7 +71,7 @@ internal class FileWatcher : PeriodicTimerContainer {
 	protected override Task Tick(CancellationToken cToken) {
 		FileTarget[] toProcess;
 		lock (_fileTargets) {
-			toProcess = _fileTargets.Where(f => f.HadChange).ToArray();
+			toProcess = [.. _fileTargets.Where(f => f.HadChange)];
 			if (toProcess.Length == 0)
 				return Task.CompletedTask;
 		}
@@ -82,18 +82,16 @@ internal class FileWatcher : PeriodicTimerContainer {
 			file.HadChange = false;
 
 			// Read and update
-			using (FileStream fileStream = new FileStream(file.Name, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
-				fileStream.Position = file.LastPosition;
+			using FileStream fileStream = new FileStream(file.Name, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+			fileStream.Position = file.LastPosition;
 
-				using (StreamReader reader = new StreamReader(fileStream)) {
-					string? line;
-					while ((line = reader.ReadLine()) != null) {
-						_processLine(line);
-					}
-
-					file.LastPosition = fileStream.Position;
-				}
+			using StreamReader reader = new StreamReader(fileStream);
+			string? line;
+			while ((line = reader.ReadLine()) != null) {
+				_processLine(line);
 			}
+
+			file.LastPosition = fileStream.Position;
 		}
 
 		_finishedRead();
