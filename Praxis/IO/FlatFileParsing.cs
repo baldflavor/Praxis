e28e7c8911336d561cadb617c2f6bac31a2b45ed
baldflavor <207648894+baldflavor@users.noreply.github.com>
2025-09-09 15:48:34 -0700

    Flatfile / csv parsing documentation and unquote tweak

--- Praxis/IO/FlatFileParsing.cs
+++ Praxis/IO/FlatFileParsing.cs
@@ -8,38 +8,36 @@ using System.Text.RegularExpressions;
 public static partial class FlatFileParsing {
 
 	/// <summary>
-	/// Splits out a CSV line of data into a string array of each of it's fields of data
+	/// Splits a CSV line of data into string fields of data.
 	/// </summary>
-	/// <param name="lineData">The line of data to split</param>
-	/// <param name="autoUnQuote">True to auto-remove quotes from the beginning and end of the resultant data</param>
-	/// <returns>A string array</returns>
-	public static string[] CsvLineToArray(string lineData, bool autoUnQuote) {
+	/// <param name="line">The line of data to split</param>
+	/// <param name="autoUnQuote">True to remove leading and trailing quotes from each resultant field.</param>
+	/// <returns><c>string[]</c></returns>
+	public static string[] CsvLineToArray(string line, bool autoUnQuote) {
 		Regex r = _CsvLineRegex();
-		if (autoUnQuote == false)
-			return r.Split(lineData);
 
-		string[] fields = r.Split(lineData);
-		for (int i = 0; i < fields.Length; i++) {
-			fields[i] = UnQuoteField(fields[i]);
+		string[] fields = r.Split(line);
+		if (autoUnQuote == false) {
+			for (int i = 0; i < fields.Length; i++) {
+				fields[i] = UnQuote(fields[i]);
+			}
 		}
 
 		return fields;
 	}
 
 	/// <summary>
-	/// Removes the quotes from the beginning and end of a string if it has quotation marks at
-	/// the beginning and end of it
+	/// Removes <c>"</c> from the beginning and end of a string when it leads and trails with <c>"</c>.
 	/// </summary>
-	/// <param name="arg">The arg string to unquote</param>
-	/// <returns>The string with quotes removed, otherwise just the arg</returns>
-	public static string UnQuoteField(string arg) {
-		if (string.IsNullOrEmpty(arg))
+	/// <param name="arg">Target string</param>
+	/// <returns><c>string</c></returns>
+	public static string UnQuote(string arg) {
+		if (arg.StartsWith('"') && arg.EndsWith('"')) {
+			return new string(arg.AsSpan()[1..][..^1]);
+		}
+		else {
 			return arg;
-
-		if (arg.Length < 2)
-			return arg;
-
-		return arg.StartsWith('\\') && arg.EndsWith('\\') ? arg.Remove(arg.Length - 1, 1).Remove(0, 1) : arg;
+		}
 	}
 
 	[GeneratedRegex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))")]
