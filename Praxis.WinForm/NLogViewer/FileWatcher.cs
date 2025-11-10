dfc1a3387e555dea0eb3ae66c31bf9934215e5fb
baldflavor <207648894+baldflavor@users.noreply.github.com>
2025-11-10 15:21:59 -0700

    NLogView Form updated Simple Test form added

--- Praxis.WinForm/NLogViewer/FileWatcher.cs
+++ Praxis.WinForm/NLogViewer/FileWatcher.cs
@@ -13,11 +13,9 @@ using System.Threading.Tasks;
 /// <param name="tickFrequency">How often to process file target changes.</param>
 /// <param name="startupDelay">Delay in initial startup to process changes / existing files.</param>
 /// <param name="pollingLoopOnException">Delegate for errors during the polling loop.</param>
-/// <param name="processLine">Used to signal that there is a line of text to be processed.</param>
-/// <param name="startingRead">Delegate called before reading lines from files.</param>
-/// <param name="finishedRead">Delegate called after reading lines from files has finished.</param>
+/// <param name="processLines">Used to signal that there are lines of text to be processed.</param>
 #pragma warning disable CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
-internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan startupDelay, Action<Exception> pollingLoopOnException, Action<string> processLine, Action startingRead, Action finishedRead) : PeriodicTimerContainer(tickFrequency, startupDelay, pollingLoopOnException) {
+internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan startupDelay, Action<Exception> pollingLoopOnException, Action<Queue<string>> processLines) : PeriodicTimerContainer(tickFrequency, startupDelay, pollingLoopOnException) {
 #pragma warning restore CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
 
 	/// <summary>
@@ -25,8 +23,8 @@ internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan st
 	/// </summary>
 	private readonly Dictionary<string, long> _filePositions = [];
 
-	protected override Task Tick(CancellationToken cToken) {
-		bool startedRead = false;
+	protected override async Task Tick(CancellationToken cToken) {
+		Queue<string> lines = [];
 
 		foreach (var fileInfo in _RecursiveInDirectory(directory)) {
 			string fullName = fileInfo.FullName;
@@ -36,11 +34,6 @@ internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan st
 			if (curPosition == length)
 				continue;
 
-			if (!startedRead) {
-				startedRead = true;
-				startingRead();
-			}
-
 			try {
 				// Read and update
 				using var fileStream = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
@@ -48,9 +41,8 @@ internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan st
 
 				using var reader = new StreamReader(fileStream);
 				string? line;
-				while ((line = reader.ReadLine()) != null) {
-					processLine(line);
-				}
+				while ((line = (await reader.ReadLineAsync(cToken).ConfigureAwait(false))) is not null)
+					lines.Enqueue(line);
 
 				_filePositions[fullName] = fileStream.Position;
 			}
@@ -59,10 +51,8 @@ internal class FileWatcher(string directory, TimeSpan tickFrequency, TimeSpan st
 			}
 		}
 
-		if (startedRead)
-			finishedRead();
-
-		return Task.CompletedTask;
+		if(lines.Count > 0)
+			processLines(lines);
 	}
 
 	/* ----------------------------------------------------------------------------------------------------------
