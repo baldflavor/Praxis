7d1024f9baa0554ff3cb5d84442af2ac1a2c2b80
baldflavor <207648894+baldflavor@users.noreply.github.com>
2025-11-14 11:31:43 -0700

    Removed ToLower on bool extension.

--- Praxis/Extension.Boolean.cs
+++ Praxis/Extension.Boolean.cs
@@ -12,14 +12,6 @@ public static partial class Extension {
 	/// <returns>1 or 0</returns>
 	public static int ToInteger(this bool arg) => arg ? Const.TRUEINT : Const.FALSEINT;
 
-
-	/// <summary>
-	/// Returns the invariant lowercase value of "True" or "False"
-	/// </summary>
-	/// <param name="arg">Boolean value to examine</param>
-	/// <returns>"true" or "false"</returns>
-	public static string ToStringLower(this bool arg) => arg.ToString().ToLowerInvariant();
-
 	/// <summary>
 	/// Returns either "Yes" or "No" based on true/false respectively
 	/// </summary>
