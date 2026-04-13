e77b47d742a2a045c5a988704216f35e0bf98708
baldflavor <207648894+baldflavor@users.noreply.github.com>
2026-04-13 14:46:02 -0700

    font / sizing for nlog viewer form

--- Praxis.WinForm/NLogViewer/NLogViewerForm.Designer.cs
+++ Praxis.WinForm/NLogViewer/NLogViewerForm.Designer.cs
@@ -24,20 +24,17 @@ partial class NLogViewerForm {
 	/// the contents of this method with the code editor.
 	/// </summary>
 	private void InitializeComponent() {
-		FlowLayoutPanel _optionsFlowLayoutPanel;
-		Panel _filterPanel;
-		var resources = new System.ComponentModel.ComponentResourceManager(typeof(NLogViewerForm));
+		_optionsFlowLayoutPanel = new FlowLayoutPanel();
 		_expandNewCheckBox = new CheckBox();
 		_collapseButton = new Button();
 		_clearButton = new Button();
+		_filterPanel = new Panel();
 		_filterTextBox = new TextBox();
 		_clearFilterButton = new Button();
 		_allEntriesTreeView = new TreeView();
 		_statusPanel = new Panel();
 		_statusLabel = new Label();
 		_filteredTreeView = new TreeView();
-		_optionsFlowLayoutPanel = new FlowLayoutPanel();
-		_filterPanel = new Panel();
 		_optionsFlowLayoutPanel.SuspendLayout();
 		_filterPanel.SuspendLayout();
 		_statusPanel.SuspendLayout();
@@ -52,17 +49,20 @@ partial class NLogViewerForm {
 		_optionsFlowLayoutPanel.Controls.Add(_filterPanel);
 		_optionsFlowLayoutPanel.Dock = DockStyle.Top;
 		_optionsFlowLayoutPanel.Location = new Point(0, 0);
+		_optionsFlowLayoutPanel.Margin = new Padding(0);
 		_optionsFlowLayoutPanel.Name = "_optionsFlowLayoutPanel";
-		_optionsFlowLayoutPanel.Padding = new Padding(6, 6, 0, 0);
+		_optionsFlowLayoutPanel.Padding = new Padding(6, 6, 6, 0);
 		_optionsFlowLayoutPanel.Size = new Size(737, 49);
 		_optionsFlowLayoutPanel.TabIndex = 2;
 		_optionsFlowLayoutPanel.WrapContents = false;
 		// 
 		// _expandNewCheckBox
 		// 
-		_expandNewCheckBox.Location = new Point(9, 9);
+		_expandNewCheckBox.AutoSize = true;
+		_expandNewCheckBox.Location = new Point(9, 15);
+		_expandNewCheckBox.Margin = new Padding(3, 9, 0, 0);
 		_expandNewCheckBox.Name = "_expandNewCheckBox";
-		_expandNewCheckBox.Size = new Size(91, 32);
+		_expandNewCheckBox.Size = new Size(91, 19);
 		_expandNewCheckBox.TabIndex = 0;
 		_expandNewCheckBox.TabStop = false;
 		_expandNewCheckBox.Text = "Expand New";
@@ -70,8 +70,9 @@ partial class NLogViewerForm {
 		// 
 		// _collapseButton
 		// 
-		_collapseButton.Location = new Point(113, 9);
-		_collapseButton.Margin = new Padding(10, 3, 0, 3);
+		_collapseButton.AutoSize = true;
+		_collapseButton.Location = new Point(116, 9);
+		_collapseButton.Margin = new Padding(16, 3, 0, 0);
 		_collapseButton.Name = "_collapseButton";
 		_collapseButton.Size = new Size(75, 32);
 		_collapseButton.TabIndex = 1;
@@ -81,8 +82,9 @@ partial class NLogViewerForm {
 		// 
 		// _clearButton
 		// 
-		_clearButton.Location = new Point(295, 9);
-		_clearButton.Margin = new Padding(16, 3, 0, 3);
+		_clearButton.AutoSize = true;
+		_clearButton.Location = new Point(207, 9);
+		_clearButton.Margin = new Padding(16, 3, 0, 0);
 		_clearButton.Name = "_clearButton";
 		_clearButton.Size = new Size(75, 32);
 		_clearButton.TabIndex = 4;
@@ -92,13 +94,14 @@ partial class NLogViewerForm {
 		// 
 		// _filterPanel
 		// 
+		_filterPanel.AutoSize = true;
 		_filterPanel.BorderStyle = BorderStyle.Fixed3D;
 		_filterPanel.Controls.Add(_filterTextBox);
 		_filterPanel.Controls.Add(_clearFilterButton);
-		_filterPanel.Location = new Point(390, 9);
-		_filterPanel.Margin = new Padding(20, 3, 0, 3);
+		_filterPanel.Location = new Point(302, 9);
+		_filterPanel.Margin = new Padding(20, 3, 0, 0);
 		_filterPanel.Name = "_filterPanel";
-		_filterPanel.Size = new Size(337, 32);
+		_filterPanel.Size = new Size(337, 30);
 		_filterPanel.TabIndex = 5;
 		// 
 		// _filterTextBox
@@ -115,11 +118,12 @@ partial class NLogViewerForm {
 		// 
 		// _clearFilterButton
 		// 
+		_clearFilterButton.AutoSize = true;
 		_clearFilterButton.Dock = DockStyle.Left;
 		_clearFilterButton.Location = new Point(0, 0);
 		_clearFilterButton.Margin = new Padding(0);
 		_clearFilterButton.Name = "_clearFilterButton";
-		_clearFilterButton.Size = new Size(32, 28);
+		_clearFilterButton.Size = new Size(32, 26);
 		_clearFilterButton.TabIndex = 0;
 		_clearFilterButton.TabStop = false;
 		_clearFilterButton.Text = "✖️";
@@ -129,9 +133,6 @@ partial class NLogViewerForm {
 		// 
 		_allEntriesTreeView.BackColor = Color.AntiqueWhite;
 		_allEntriesTreeView.Dock = DockStyle.Fill;
-		_allEntriesTreeView.Font = new Font("Cascadia Code", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-		_allEntriesTreeView.Indent = 22;
-		_allEntriesTreeView.ItemHeight = 16;
 		_allEntriesTreeView.LineColor = Color.SaddleBrown;
 		_allEntriesTreeView.Location = new Point(0, 96);
 		_allEntriesTreeView.Name = "_allEntriesTreeView";
@@ -165,9 +166,6 @@ partial class NLogViewerForm {
 		// 
 		_filteredTreeView.BackColor = Color.AntiqueWhite;
 		_filteredTreeView.Dock = DockStyle.Fill;
-		_filteredTreeView.Font = new Font("Cascadia Code", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-		_filteredTreeView.Indent = 22;
-		_filteredTreeView.ItemHeight = 16;
 		_filteredTreeView.LineColor = Color.SaddleBrown;
 		_filteredTreeView.Location = new Point(0, 96);
 		_filteredTreeView.Name = "_filteredTreeView";
@@ -187,11 +185,11 @@ partial class NLogViewerForm {
 		this.Controls.Add(_optionsFlowLayoutPanel);
 		this.DoubleBuffered = true;
 		this.HelpButton = true;
-		this.Icon = SystemIcons.GetStockIcon(StockIconId.AutoList);
 		this.Name = "NLogViewerForm";
 		this.StartPosition = FormStartPosition.CenterScreen;
 		this.Text = "NLog Viewer (Right click any node to copy a log to the clipboard)";
 		_optionsFlowLayoutPanel.ResumeLayout(false);
+		_optionsFlowLayoutPanel.PerformLayout();
 		_filterPanel.ResumeLayout(false);
 		_filterPanel.PerformLayout();
 		_statusPanel.ResumeLayout(false);
@@ -209,4 +207,6 @@ partial class NLogViewerForm {
 	private Panel _statusPanel;
 	private Label _statusLabel;
 	private TreeView _filteredTreeView;
+	private FlowLayoutPanel _optionsFlowLayoutPanel;
+	private Panel _filterPanel;
 }
